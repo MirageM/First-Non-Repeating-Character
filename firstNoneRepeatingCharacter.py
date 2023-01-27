d67563f36e7408aae930c3f6520cde9d23dc7560
MirageM <imiragesz@gmail.com>
2023-01-27 00:02:07 -0500

    First Non Repeating Character

--- firstNoneRepeatingCharacter.py
+++ firstNoneRepeatingCharacter.py
@@ -1,4 +1,10 @@
 #First Non Repeating Character
 def firstNonRepeatingCharacter(string):
-    # Write your code here.
+    for i in range(len(string)):
+        foundDuplicate = False
+        for j in range(len(string)):
+            if string[i] == string[j] and i != j:
+                foundDuplicate = True
+        if not foundDuplicate:
+            return i
     return -1
