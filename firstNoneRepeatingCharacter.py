f2a3fe229048372585e8fcd83d76a630b9f5a5fd
MirageM <imiragesz@gmail.com>
2023-01-28 18:49:14 -0500

    firstNoneRepeatingCharacter.py

--- firstNoneRepeatingCharacter.py
+++ firstNoneRepeatingCharacter.py
@@ -1,4 +1,12 @@
 #First Non-Repeating Character
+#Brute Force Method -> Looks at every character
+#O(n^2) time | O(1) space
 def firstNonRepeatingCharacter(string):
-    # Write your code here.
+    for i in range(len(string)):
+        foundDuplicate = False
+        for j in range(len(string)):
+            if string[i] == string[j] and i != j:
+                foundDuplicate = True
+        if not foundDuplicate:
+            return i
     return -1
